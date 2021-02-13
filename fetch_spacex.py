ee2a99176b3d7add9ed917d8500ced97d4bc5c37
Wersario <60400132+Wersario@users.noreply.github.com>
2021-02-13 17:30:59 +0300

    Divided functions and main file

---
+++ fetch_spacex.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from PIL import Image
+
+
+def get_link_expansion(link):
+    split_link = link.split('.')
+    return '.' + split_link[-1]
+
+
+def resize_picture(path):
+    image = Image.open(path)
+    image.thumbnail((1080, 1080))
+    image.convert('RGB').save(f'{path.split(".")[0]}.jpg')
+    if get_link_expansion(path) != '.jpg':
+        os.remove(path)
+
+
+def download_image(url, path):
+    os.makedirs("images", exist_ok=True)
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+    with open(path, "wb") as file:
+        file.write(response.content)
+
+
+def fetch_spacex_last_launch():
+    os.makedirs("images", exist_ok=True)
+    response = requests.get("https://api.spacexdata.com/v4/launches/latest")
+    response.raise_for_status()
+    for count, i in enumerate(response.json()['links']['flickr']['original']):
+        download_image(i, f'images/spacex{count}.jpg')
+        resize_picture(f'images/spacex{count}.jpg')
+
+
+fetch_spacex_last_launch()
