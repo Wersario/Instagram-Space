8e6e5579585746e805a59cae674cb67ccf051474
Wersario <60400132+Wersario@users.noreply.github.com>
2021-02-14 14:19:50 +0300

    Fixed name of the variable

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -28,8 +28,8 @@ def fetch_spacex_last_launch():
     os.makedirs("images", exist_ok=True)
     response = requests.get("https://api.spacexdata.com/v4/launches/latest")
     response.raise_for_status()
-    for count, i in enumerate(response.json()['links']['flickr']['original']):
-        download_image(i, f'images/spacex{count}.jpg')
+    for count, link in enumerate(response.json()['links']['flickr']['original']):
+        download_image(link, f'images/spacex{count}.jpg')
         resize_picture(f'images/spacex{count}.jpg')
 
 
